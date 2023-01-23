023a94b092f301571deda9296d642bd0ab8b4f08
vishalicious213 <vishalicious213@gmail.com>
2023-01-23 15:46:13 -0800

    added event listener to form. usued it to prevendDefault() so that form data isn't submitted and page isn't refreshed (and query string isn't added to URL)

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const modal = document.getElementById("modal")
 const closeBtn = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
 
 // make modal appear after 1.5 seconds
 setTimeout(function() {
     modal.style.display = "inline"
 }, 1500)
 
+// make modal disappear after "X" is clicked
 closeBtn.addEventListener("click", function() {
     modal.style.display = "none"
+})
+
+// prevent default submit/refresh behavior on form
+consentForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    console.log("Form submitted")
 })
