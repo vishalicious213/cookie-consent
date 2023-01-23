fb7151d55a7bf138424318e572038b98e4ebd786
vishalicious213 <vishalicious213@gmail.com>
2023-01-23 10:36:49 -0800

    modal pops up after 1.5 seconds

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("cookie consent")
+const modal = document.getElementById("modal")
+
+// make modal appear after 1.5 seconds
+setTimeout(function() {
+    modal.style.display = "inline"
+}, 1500)
