1655242879bddba7963b85d843a16395f263815b
vishalicious213 <vishalicious213@gmail.com>
2023-01-23 10:42:39 -0800

    made modal close button work

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const modal = document.getElementById("modal")
+const closeBtn = document.getElementById("modal-close-btn")
 
 // make modal appear after 1.5 seconds
 setTimeout(function() {
     modal.style.display = "inline"
 }, 1500)
+
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none"
+})
