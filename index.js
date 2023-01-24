95134bdd623a08fd8448b980046ec0b75a53f37a
vishalicious213 <vishalicious213@gmail.com>
2023-01-23 16:28:22 -0800

    changeded modal innerHTML 1.5 seconds after previous change. added pirate image and message

--- index.js
+++ index.js
@@ -29,4 +29,14 @@ consentForm.addEventListener("submit", function(e) {
     setTimeout(function() {
         document.getElementById('upload-text').textContent = `Making the sale...`
     }, 1500)
+
+    setTimeout(function() {
+        document.getElementById('modal-inner').innerHTML =    `
+            <h2>Thanks you sucker!</h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/pirate.gif">
+            </div>
+        `
+    }, 3000)
 })
