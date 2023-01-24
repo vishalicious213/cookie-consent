426f4ac226f44a2ae06ca4ab3ddeb2c69d216b7a
vishalicious213 <vishalicious213@gmail.com>
2023-01-23 16:04:48 -0800

    replaced modal text when form is submitted to show user that their data is being submitted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal")
 const closeBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
 
 // make modal appear after 1.5 seconds
 setTimeout(function() {
@@ -16,4 +17,12 @@ closeBtn.addEventListener("click", function() {
 consentForm.addEventListener("submit", function(e) {
     e.preventDefault()
     console.log("Form submitted")
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="uploadText">
+                Uploading your data to the dark web...
+            </p>
+        </div>
+    `
 })
